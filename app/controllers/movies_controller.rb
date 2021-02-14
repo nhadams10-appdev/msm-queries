aa56648e3160ee55ae3e03e5529518c444ebe667
Noah Adams <nhadams10@uchicago.edu>
2021-02-14 21:35:54 +0000

    Finished movies :)

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+
+  def index
+    @list_of_movies = Movie.all
+
+    render({ :template => "movie_templates/index.html.erb"})
+  end
+
+  def movie_details
+    the_id = params.fetch("an_id")
+
+    @the_movie = Movie.where({ :id => the_id }).first
+    #@filmography = Movie.where({ :director_id => the_id })
+
+    render({ :template => "movie_templates/details.html.erb"})
+  end
+
+end
