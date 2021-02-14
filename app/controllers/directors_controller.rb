1b9ba28cd0d7ba3fab18dc713ecdcd767f89505d
Noah Adams <nhadams10@uchicago.edu>
2021-02-14 20:24:42 +0000

    Youngest and oldest done, started on director details

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,4 +6,24 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/index.html.erb"})
   end
 
+  def wisest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).first
+  
+    render({ :template => "director_templates/eldest.html.erb"})
+  end
+ 
+  def spryest
+    @youngest = Director.order({ :dob => :desc }).first
+  
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
+
+  def director_details
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id }).first
+
+    render({ :template => "director_templates/details.html.erb"})
+  end
+
 end
