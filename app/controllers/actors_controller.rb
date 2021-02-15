be80db4e7c4b6ea43bea7476c859bd059047cfb1
Noah Adams <nhadams10@uchicago.edu>
2021-02-15 21:02:45 +0000

    Finished!

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -10,7 +10,7 @@ class ActorsController < ApplicationController
     the_id = params.fetch("an_id")
 
     @the_actor = Actor.where({ :id => the_id }).first
-    @filmography = Movie.where({ :director_id => the_id })
+    @filmography = Character.where({ :actor_id => the_id })
 
     render({ :template => "actor_templates/details.html.erb"})
   end
